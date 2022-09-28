07c96c288773957faee89328b5b9964ccc904094
Vuk <vuk.vladisavljevic@htecgroup.com>
2022-09-28 10:51:45 +0200

    Make use of env variables

--- src/index.js
+++ src/index.js
@@ -14,20 +14,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('6331ab2f9e5c576fe3af9bc4')
-    // await task.populate('owner')
-    // console.log(task.owner)
-
-    // const user = await User.findById('6331aa3114420ce29644a083')
-    // await user.populate('tasks')
-    // console.log(user.tasks);
-
-
-}
-
-main()
